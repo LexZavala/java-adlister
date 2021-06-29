1cdfa223954771b27515a52c187bbdc4ba54e086
Lex Zavala <lex.zavala7@gmail.com>
2021-06-29 14:02:33 -0500

    completed  exercise

--- src/main/java/HelloWorldServlet.java
+++ src/main/java/HelloWorldServlet.java
@@ -1,3 +1,28 @@
+//import javax.servlet.annotation.WebServlet;
+//import javax.servlet.http.HttpServlet;
+//import javax.servlet.http.HttpServletRequest;
+//import javax.servlet.http.HttpServletResponse;
+//import java.io.IOException;
+//import java.io.PrintWriter;
+//
+//@WebServlet(name= "HelloWorldServlet", urlPatterns = "/hello-world")
+//public class HelloWorldServlet extends HttpServlet {
+//
+//    @Override
+//    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+//        String name = req.getParameter("name");
+//        res.setContentType("text/html");
+//        PrintWriter out = res.getWriter();
+//
+//        if (name == null) {
+//            out.println("<h1>Hello, World</h1>");
+//        } else {
+//            out.println("<h1> Hello " + name + "!</h1>");
+//        }
+//    }
+//}
+///////////////// LECTURE
+
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -5,19 +30,19 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 import java.io.PrintWriter;
 
-@WebServlet(name= "HelloWorldServlet", urlPatterns = "/hello-world")
+@WebServlet(name = "HelloWorldServlet2", urlPatterns = "/hello")
 public class HelloWorldServlet extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse res) throws IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
         String name = req.getParameter("name");
-        res.setContentType("text/html");
-        PrintWriter out = res.getWriter();
+        resp.setContentType("text/html");
+        PrintWriter out = resp.getWriter();
 
-        if (name == null) {
-            out.println("<h1>Hello, World</h1>");
+        if (name == null){
+            out.println("Hello World!");
         } else {
-            out.println("<h1> Hello " + name + "!</h1>");
+            out.println("Hello " + name + "!");
         }
     }
 }
