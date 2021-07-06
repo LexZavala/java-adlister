de4b737d43b3c9925c59266bf3496caf0944245c
Lex Zavala <lex.zavala7@gmail.com>
2021-07-06 15:26:18 -0500

    completed lecture walkthrough

--- src/main/java/DaoFactory.java
+++ src/main/java/DaoFactory.java
@@ -1,10 +1,19 @@
 public class DaoFactory {
     private static Ads adsDao;
+    private static Products productsDao;
 
     public static Ads getAdsDao() {
         if (adsDao == null) {
             adsDao = new ListAdsDao();
         }
         return adsDao;
+    }
+
+    public static Products getProductsDao(){
+//      Singleton pattern, making sure there is a single creation
+        if(productsDao == null) {
+            productsDao = new ListProducts();
+        }
+        return productsDao;
     }
 }
