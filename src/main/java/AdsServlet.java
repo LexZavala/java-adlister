9a000ea2e3f184682a54dbe6ceb98b7920d6edca
Lex Zavala <lex.zavala7@gmail.com>
2021-07-06 22:19:17 -0500

    completed exercise

--- src/main/java/AdsServlet.java
+++ src/main/java/AdsServlet.java
@@ -4,16 +4,18 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.List;
 
 @WebServlet ("/ads")
 public class AdsServlet extends HttpServlet {
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+        List<Ad> listing = DaoFactory.getAdsDao().all();
+        request.setAttribute("ads", listing);
+
         request.getRequestDispatcher("/ads/index.jsp").forward(request, response);
-    }
-
-    @Override
-    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 
     }
+
 }
--- src/main/webapp/ads/index.jsp
+++ src/main/webapp/ads/index.jsp
@@ -5,9 +5,9 @@
   Time: 3:47 PM
   To change this template use File | Settings | File Templates.
 --%>
-<%
-    request.getRequestDispatcher("/login.jsp").forward(request, response);
-%>
+<%--<%--%>
+<%--    request.getRequestDispatcher("/login.jsp").forward(request, response);--%>
+<%--%>--%>
 
 <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
 <%@ page contentType="text/html;charset=UTF-8" language="java" %>
@@ -16,7 +16,15 @@
     <title>Ads Index</title>
 </head>
 <body>
-    <h1>This is your ads index</h1>
+    <h1>Here are your Ads </h1>
+
+<c:forEach var="ad" items="${ads}">
+    <div class="ad">
+        <h2>${ad.title}</h2>
+        <p>Price: $ ${ad.description}</p>
+    </div>
+</c:forEach>
+
 
 </body>
 </html>
